22c5fc885ddec113b02ebcb4d79a0e7e150ec976
Valentina-Moon <valentina.tirtik_777@mail.ru>
2022-10-25 17:56:54 +0300

    копирования массива

---
+++ task№4/Program.cs
@@ -0,0 +1,20 @@
+﻿//Написать программу копирования массива
+
+int[] n = new int[10]; 
+int[] m = new int[10]; 
+for (int i = 0; i < 10; i++)
+{
+    n[i] = i + 99;
+}
+Console.WriteLine("Оригинальный массив...");
+foreach (int j in n)
+{
+    int i = j - 99;
+    Console.WriteLine("Элемент[{0}] = {1}", i, j);
+}
+Array.Copy(n, 0, m, 0, 10);
+Console.WriteLine("Новый массив...");
+foreach (int res in m)
+{
+    Console.WriteLine(res);
+}
